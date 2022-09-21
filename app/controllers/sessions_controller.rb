1a034eb3ec412d78d62ce102eb1076e74fc66b87
dangnv121299 <nguyen.van.dang@sun-asterisk.com>
2022-09-21 14:14:07 +0700

    init_layout_login_logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  before_action :find_by_email, only: :create
+
+  def new; end
+
+  def create
+    if @user&.authenticate(params[:session][:password])
+      log_in @user
+      redirect_to root_path
+    else
+      flash.now[:danger] = t ".flash_danger"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
+
+  private
+
+  def find_by_email
+    @user = User.find_by email: params.dig(:session, :email)&.downcase
+    return if @user
+
+    flash[:danger] = t ".find_error"
+    redirect_to root_path
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :user_department
+  belongs_to :user_department, optional: true
   has_many :daily_reports, dependent: :destroy
 
   has_secure_password
@@ -11,4 +11,20 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: Settings.user.max_email},
                     format: {with: Settings.user.email_regex},
                     uniqueness: true
+  UPDATABLE_ATTRS = %i(name email password password_confirmation).freeze
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
 end
