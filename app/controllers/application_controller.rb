d47d52ac7cbbf5c874dc374feb30327975532995
dangnv121299 <nguyen.van.dang@sun-asterisk.com>
2022-09-22 20:39:21 +0700

    update_infor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
                   end
   end
 
+  def default_url_options
+    {locale: I18n.locale}
+  end
+
   def logged_in_user
     return if logged_in?
 
