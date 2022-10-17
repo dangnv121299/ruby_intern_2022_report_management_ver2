5e6b1bdb5245421e66bd4a78af7cbfcc8c7d5ebe
dangnv121299 <nguyen.van.dang@sun-asterisk.com>
2022-10-17 13:18:51 +0700

    Gem bullet

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     return unless user_signed_in?
 
-    @rps = Report.by_department_id(find_managed(current_user))
+    @rps = Report.includes(:user).by_department_id(find_managed(current_user))
     @pagy, @feed_items = pagy @rps.newest
   end
 
